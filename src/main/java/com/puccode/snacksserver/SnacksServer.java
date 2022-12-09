44be1f436c5c78c87fb50d0a70c395938d00ce21
carlosantony-dev <carlosantony07@outlook.com>
2022-12-09 00:26:24 -0300

    save server updates

--- src/main/java/com/puccode/snacksserver/SnacksServer.java
+++ src/main/java/com/puccode/snacksserver/SnacksServer.java
@@ -4,22 +4,22 @@ import com.puccode.snacksserver.forms.Home;
 import com.puccode.snacksserver.services.Services;
 import java.awt.Dimension;
 import java.io.IOException;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import java.awt.GridLayout;
 
-/**
- *
- * @author carlo
- */
 public class SnacksServer implements Runnable {
 
+    private static Home myHome;
+
     public static void main(String[] args) throws IOException, InterruptedException {
+        myHome = new Home();
         SwingUtilities.invokeLater(new SnacksServer());
 
         //André, forPort method, is very similar of Socket class: Socket socker = new Socket(address, port);
-        Server server = ServerBuilder.forPort(9090).addService(new Services()).build();
+        Server server = ServerBuilder.forPort(9090).addService(new Services(myHome)).build();
         server.start();
         System.out.println("Server started at: " + server.getPort());
 
@@ -29,7 +29,7 @@ public class SnacksServer implements Runnable {
     @Override
     public void run() {
         JFrame jFrame = new JFrame();
-        jFrame.setContentPane(new Home().getContentPane());
+        jFrame.setContentPane(myHome.getContentPane());
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.setTitle("Java Snacks Restaurant");
         jFrame.setSize(new Dimension(1080, 720));
--- src/main/java/com/puccode/snacksserver/services/Services.java
+++ src/main/java/com/puccode/snacksserver/services/Services.java
@@ -1,5 +1,6 @@
 package com.puccode.snacksserver.services;
 
+import com.puccode.snacksserver.forms.Home;
 import io.grpc.stub.StreamObserver;
 import stubs.APIResponse;
 import stubs.Order;
@@ -12,6 +13,11 @@ import java.util.Queue;
 public class Services extends clientGrpc.clientImplBase {
 
     private Queue<Order> pedidos = new LinkedList<>();
+    private final Home myHome;
+
+    public Services(Home home) {
+        myHome = home;
+    }
 
 
     @Override
@@ -21,9 +27,10 @@ public class Services extends clientGrpc.clientImplBase {
         if (request != null) {
             pedidos.add(request);
         }
-
+        
+        myHome.refreshOrders(pedidos);
         APIResponse.Builder response = APIResponse.newBuilder();
-        response.setMessageCode(0).setResponseMessage("SUCESS");
+        response.setMessageCode(200).setResponseMessage("Pedido solicitado com sucesso!");
 
         responseObserver.onNext(response.build());
         responseObserver.onCompleted();
@@ -31,6 +38,25 @@ public class Services extends clientGrpc.clientImplBase {
 
     @Override
     public void cancelOrder(OrderID request, StreamObserver<APIResponse> responseObserver) {
+        APIResponse.Builder response = APIResponse.newBuilder();
 
+        if (request != null) {
+            if (!pedidos.isEmpty()) {
+                int pos = 0;
+                for (Order ord : pedidos) {
+                    if (pos == request.getOrderNumber()) {
+                        pedidos.remove(ord);
+                    }
+                    pos++;
+                }
+                response.setMessageCode(200).setResponseMessage("Pedido cancelado com sucesso!");
+            } else {
+                response.setMessageCode(404).setResponseMessage("A lista de pedidos está vázia! Não há o que cancelar.");
+            }
+        }
+        myHome.refreshOrders(pedidos);
+
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
     }
 }
